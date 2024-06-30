725f1af483ffe5e9ce34f11a182eaf2c29de3bc4
Mayank Singh <mt029322@gmail.com>
2024-06-30 16:10:39 +0530

    cors annotation

--- backend/RentalProject/src/main/java/com/example/otp/OtpController.java
+++ backend/RentalProject/src/main/java/com/example/otp/OtpController.java
@@ -1,9 +1,11 @@
 package com.example.otp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class OtpController {
 
